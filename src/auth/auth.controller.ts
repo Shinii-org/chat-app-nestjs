81ad7e292d1dbaedbc81b52fca2dfd2673805ffa
ngocphu1996 <ngocphu280996@gmail.com>
2022-09-11 19:17:48 +0700

    implement register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,32 @@
-import { Controller, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  Post,
+  Get,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+
 import { Routes, Services } from '../utils/types';
 import { IAuthService } from './auth';
+import { CreateUserDto } from './dtos/CreateUser.dto';
 
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(@Inject(Services.AUTH) private authService: IAuthService) {}
+
+  @Post('register')
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+  }
+
+  @Post('login')
+  login() {}
+
+  @Get('status')
+  status() {}
+
+  @Post('logout')
+  logout() {}
 }
---
+++ src/auth/dtos/CreateUser.dto.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsNotEmpty, MaxLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MaxLength(32)
+  firstName: string;
+
+  @IsNotEmpty()
+  @MaxLength(32)
+  lastName: string;
+
+  @IsNotEmpty()
+  @MaxLength(32)
+  password: string;
+}
